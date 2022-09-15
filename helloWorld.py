48dd134f9ce0892d9d570f7365aa9468c5e3a70a
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-09-15 15:00:31 +0000

    Added code to hello_world.py

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+# Created by: Kaitlyn Ip
+# Created on: September 2022
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
